d99f1ebd27d64f7c9f296bba15587497513e6add
Brad Cowie <brad@wand.net.nz>
2021-11-22 14:29:41 +1300

    Initial commit.

---
+++ publish.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -euo pipefail
+
+github_api_url="https://api.github.com/repos/twpayne/chezmoi/releases/latest"
+cloudsmith_config_url="https://dl.cloudsmith.io/public/wand/chezmoi/config.deb.txt?distro=$(lsb_release -is)&codename=$(lsb_release -sc)"
+
+deb_repo_url=$(curl -1sLf "${cloudsmith_config_url}" | grep "deb " | awk '{print $2}')
+
+mapfile -t deb_files < <(curl -1sLf "${github_api_url}" 2>/dev/null \
+    | jq -r '.assets[] | select(.name | endswith(".deb")) | [.name, .browser_download_url] | @sh')
+
+new_packages=0
+for deb_file in "${deb_files[@]}"; do
+    pkg_filename=$(echo "${deb_file}" | awk '{print $1}' | xargs)
+    pkg_url=$(echo "${deb_file}" | awk '{print $2}' | xargs)
+
+    curl -1sLf "${pkg_url}" -o "${GITHUB_WORKSPACE}/${pkg_filename}"
+
+    pkg_arch=$(dpkg -I "${GITHUB_WORKSPACE}/${pkg_filename}" | grep Architecture | awk '{print $2}')
+
+    deb_repo_packages_url="${deb_repo_url}/dists/$(lsb_release -sc)/main/binary-${pkg_arch}/Packages"
+
+    package_path="${GITHUB_WORKSPACE}/packages/any-distro_any-version/"
+    mkdir -p "${package_path}"
+
+    if ! curl -1sLf "${deb_repo_packages_url}" 2>&1 | grep "${pkg_filename}" > /dev/null; then
+        mv "${GITHUB_WORKSPACE}/${pkg_filename}" "${package_path}/${pkg_filename}"
+        new_packages=$((new_packages+=1))
+    fi
+done
+
+echo "NEW_PACKAGES=${new_packages}" >> "${GITHUB_ENV}"
